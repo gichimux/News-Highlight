f75ee6c161142dde9bc047a33094d3ba8a84abb2
gichimux <gichimumwai1@gmail.com>
2019-04-16 10:58:22 +0300

    corrected naming error bug

--- app/main/views.py
+++ app/main/views.py
@@ -1,6 +1,6 @@
 from flask import render_template, request, redirect, url_for
 from . import main
-from ..request import get_articles, get_news
+from ..requests import get_articles, get_news
 
 
 #views
@@ -12,16 +12,16 @@ def index():
     title = "Home || News Sources"
 
     all_news = get_news('general')
-    general_news = get_news('sports')
-    tech_news = get_news('business')
-    bus_news = get_news('science')
-    sci_news = get_news('technology')
+    sports_news = get_news('sports')
+    science_news = get_news('science')
+    business_news = get_news('business')
+    technology_news = get_news('technology')
 
 
     # print(all_news)
 
 
-    return render_template('index.html', title= title, sports = all_news, general = general_news, technology = tech_news, business = bus_news, science = sci_news)
+    return render_template('index.html', title=title, sports=sports_news, general=all_news, technology=technology_news, business=business_news, science=science_news)
 
 # Views
 @main.route('/news/<int:id>')
@@ -44,4 +44,4 @@ def articles(source_id):
 
     article_source = get_articles(source_id)
     title = f'{source_id}| Articles'
-    return render_template('articles.html',title = title, name = source_id, news = article_source )
+    return render_template('articles.html',title =title, name=source_id, news=article_source )
